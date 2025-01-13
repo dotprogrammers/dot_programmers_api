686ba885c31cdf9a4b79ca419e3b28f533456bed
dreammehedi <dreammehedihassan@gmail.com>
2025-01-13 14:13:07 +0600

    update

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -5,12 +5,12 @@ export const connectDb = async () => {
     const connectionInstance = await mongoose.connect(
       `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.sizskqa.mongodb.net/${process.env.DB_NAME}`
     );
-    console.log(
-      "Database Connected Successfully.",
-      connectionInstance.connection.host
-    );
+    // console.log(
+    //   "Database Connected Successfully.",
+    //   connectionInstance.connection.host
+    // );
   } catch (error) {
-    console.log("Database Connection Error!", error);
+    // console.log("Database Connection Error!", error);
     process.exit(1);
   }
 };
