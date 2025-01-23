ed94f949c4bd644071d526f0dac5615c377a1552
dreammehedi <dreammehedihassan@gmail.com>
2025-01-23 22:50:16 +0600

    update server

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ dotenv.config({
 // Connect to the database and start the server
 connectDb()
   .then(() => {
-    console.log("Database connection successful!");
+    // console.log("Database connection successful!");
 
     // Start the server
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server Running On PORT: ${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`Server Running On PORT: ${PORT}`);
+    // });
+
+    app.listen();
   })
   .catch((error) => {
     console.error("Database Server Connection Error:", error);
