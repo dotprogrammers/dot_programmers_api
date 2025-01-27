f2adcc00c3905a443712342316fa1a6f108f6f2d
dreammehedi <dreammehedihassan@gmail.com>
2025-01-27 22:08:22 +0600

    update db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.sizskqa.mongodb.net/${process.env.DB_NAME}`
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@clustermehedi.v8xdn.mongodb.net/${process.env.DB_NAME}`
     );
     // console.log(
     //   "Database Connected Successfully.",
