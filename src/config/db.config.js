739c9677e48b0d049d5299cf016c6deb519a33eb
dotprogrammers <dotprogrammers@gmail.com>
2025-07-23 06:12:40 +0600

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,8 +3,10 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@clustermehedi.v8xdn.mongodb.net/${process.env.DB_NAME}`
+     // `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@clustermehedi.v8xdn.mongodb.net/${process.env.DB_NAME}`
+       `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.bljc97k.mongodb.net/${process.env.DB_NAME}`
     );
+     //mongoimport --uri  mongodb+srv://dotprogrammers:<enter_password>@cluster0.bljc97k.mongodb.net/<enter_database_name>  --collection  <enter_collection_name>  --type  JSON    --file  <enter path/to/file>
     // console.log(
     //   "Database Connected Successfully.",
     //   connectionInstance.connection.host
