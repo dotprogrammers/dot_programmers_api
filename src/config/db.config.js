2d424d9afc0750a6af305fdfed29e752d5e76361
dotprogrammers <dotprogrammers@gmail.com>
2025-07-23 06:28:16 +0600

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -7,12 +7,12 @@ export const connectDb = async () => {
        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.bljc97k.mongodb.net/${process.env.DB_NAME}`
     );
      //mongoimport --uri  mongodb+srv://dotprogrammers:<enter_password>@cluster0.bljc97k.mongodb.net/<enter_database_name>  --collection  <enter_collection_name>  --type  JSON    --file  <enter path/to/file>
-    // console.log(
-    //   "Database Connected Successfully.",
-    //   connectionInstance.connection.host
-    // );
+    console.log(
+      "Database Connected Successfully.",
+      connectionInstance.connection.host
+    );
   } catch (error) {
-    // console.log("Database Connection Error!", error);
+     console.log("Database Connection Error!", error);
     process.exit(1);
   }
 };
